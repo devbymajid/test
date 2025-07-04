d9286029aef1e29de5c836fcdb598861f61f1df2
Abdelmajid Ben driss <deromajid97@gmail.com>
2025-07-04 11:43:41 +0000

    ok

--- test 2/js.js
+++
@@ -1,8 +0,0 @@
-
-  function toggleSearch() {
-    const input = document.getElementById("searchInput");
-    input.classList.toggle("show");
-    if (input.classList.contains("show")) {
-      input.focus();
-    }
-  }
