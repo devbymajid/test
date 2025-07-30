67f3e9cf7078b2ea1628ea740f7c809bf7d0b5ef
Abdelmajid Ben driss <deromajid97@gmail.com>
2025-07-30 15:28:44 +0000

    ok

---
+++ js/js.js
@@ -0,0 +1,29 @@
+/* let num = 10;
+num = num + 1;
+console.log(num);
+document.write(++num);*/
+
+/*console.log(Number.MAX_SAFE_INTEGER) ;
+console.log(Number.MIN_SAFE_INTEGER) ;*/
+/*let dero = document.writeln('<input type="number"  id="dero">');
+dero = document.writeln('<button id="btn">Click</button>');
+document.getElementById('btn').addEventListener('click', function() {
+    dero = document.getElementById('dero').value;
+    dero = parseFloat(dero);
+    if (isNaN(dero)) {
+        alert('Please enter a valid number');
+    } else {
+
+        document.write(1 + dero + ' $');
+    }
+});*/
+let name = 'Dero majid';
+
+console.log(name[30]);
+console.log(name.slice('dero'));
+
+
+
+
+
+
